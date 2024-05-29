e383d62d2f8519f320d7682151396e8cb116d3aa
vleandrox <victor.alarcon.t@hotmail.com>
2024-05-29 11:52:37 -0500

    FinishUpdatVistas

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import Home from './components/Home';
+import Details from './views/Details';
+import Cart from './views/Cart';
 
 function App() {
   return (
-    <Home></Home>
+    <Cart></Cart>
   );
 }
 
